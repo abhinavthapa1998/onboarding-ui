78a321633b9a0d2f30a9c4726ee34946038bba6d
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-08-17 06:07:20 +0000

    Components Created

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import Cards from "./components/Cards";
+import Form from "./components/Form";
+import ProgressBar from "./components/ProgressBar";
+import Title from "./components/Title";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="container">
       <h1>Logo - Eden</h1>
-      <h1>Progress Number UI</h1>
-      <h1>Heading</h1>
-      <h2>Subheading</h2>
-      <h1>Card Container</h1>
-      <div><span>Card 1</span><span>Card 2</span></div>
+      <ProgressBar/>
+      <Title/>
+      <Form/>
+      <Cards/>
     <button>Button</button>
     </div>
   );
