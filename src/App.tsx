29d29474260aac2768e94001d812757e6537673c
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-08-17 05:57:21 +0000

    Naming Structures

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <h1>Logo - Eden</h1>
+      <h1>Progress Number UI</h1>
+      <h1>Heading</h1>
+      <h2>Subheading</h2>
+      <h1>Card Container</h1>
+      <div><span>Card 1</span><span>Card 2</span></div>
+    <button>Button</button>
     </div>
   );
 }
