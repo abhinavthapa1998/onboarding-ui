f4631cc58aaee89c988f24b0ad6c69aa664c6b01
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-08-18 20:09:43 +0530

    Cards

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,13 +1,23 @@
-import React from "react";
 import Card from "./Card";
-
+import Person from "../assets/icons/person.svg";
+import People from "../assets/icons/people.svg";
 type Props = {};
 
 function Cards({}: Props) {
   return (
     <div className="cardsContainer">
-      <Card />
-      <Card />
+      <Card
+        icon={Person}
+        heading="For myself"
+        subHeading="Write better. Think more clearly. Stay organized."
+        active={false}
+      />
+      <Card
+        icon={People}
+        heading="With my team"
+        subHeading="Wikis, docs, tasks & projects, all in one place."
+        active={true}
+      />
     </div>
   );
 }
